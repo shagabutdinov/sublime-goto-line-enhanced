4653cdac4249ea422260162b2f864b789991e6b9
Leonid Shagabutdinov <leonid@shagabutidnov.com>
2014-10-03 12:06:06 +0700

    initial

---
+++ commands.py
@@ -0,0 +1,149 @@
+import sublime
+import sublime_plugin
+import re
+from QuickSearchEnhanced.quick_search import panels
+
+class PromptGotoLine(sublime_plugin.TextCommand):
+  def run(self, edit, start_with_current = False):
+    label = 'Tab for goto; Esc for cancel'
+
+    current = ''
+    if start_with_current and len(self.view.sel()) > 0:
+      current, _ = self.view.rowcol(self.view.sel()[0].a)
+      current += 1
+
+    panels.create([label], None, self._close, None, str(current),
+      [['goto_line', True]], self._create).show()
+
+    self.initial_viewport_position = self.view.viewport_position()
+
+  def _close(self, panel):
+    self.view.show(self.view.sel()[0])
+    if not getattr(panel, 'success', False):
+      self.view.set_viewport_position(self.initial_viewport_position)
+
+    callback = lambda: panel.get_opener().erase_regions('goto_line_enhanced')
+    sublime.set_timeout(callback, 100)
+
+  def _create(self, panel):
+    view = panel.get_panel()
+    view.sel().clear()
+    view.sel().add(sublime.Region(0, 0))
+    view.set_overwrite_status(True)
+
+def get_panel():
+  panel = panels.get_current()
+  goto_panel = panel and panel.get_caller('goto_line')
+  if goto_panel == None:
+    return None
+
+  return panel
+
+def convert_query_to_point(view, query, left = False):
+  if re.match(r'\d+', query) == None:
+    return
+
+  query = re.sub(r'\D', '', query)
+  max_line, _ = view.rowcol(view.size())
+  line = int(query) - 1
+  if line > max_line:
+    line = max_line
+
+  point = view.text_point(line, 0)
+  string = view.substr(view.line(point))
+
+  if left:
+    point = view.line(point).b
+  else:
+    shift_match = re.search(r'(?<!\n)(\S|$)', string)
+    if shift_match != None:
+      point += shift_match.start(1)
+
+  return point
+
+def convert_query_to_region(view, query):
+  start = end = convert_query_to_point(view, query)
+
+  if start == None or end == None:
+    return None
+
+  return sublime.Region(start, end)
+
+class GotoLineComplete(sublime_plugin.TextCommand):
+  def run(self, edit):
+    panel = get_panel()
+    if panel == None:
+      return
+
+    view, query = panel.get_opener(), panel.get_current_text()
+    region = convert_query_to_region(view, query)
+    view.sel().clear()
+    view.sel().add(region)
+    panel.close(None, False)
+    view.show(region)
+    setattr(panel, 'success', True)
+
+class GotoLineInsertZero(sublime_plugin.TextCommand):
+  def run(self, edit):
+    self.view.insert(edit, 0, '0')
+
+class GotoLineFilterQuery(sublime_plugin.TextCommand):
+  def run(self, edit):
+    panel = get_panel()
+    if panel == None:
+      return
+
+    view = panel.get_opener()
+
+    all = sublime.Region(0, self.view.size())
+    query = self.view.substr(all)
+    new_query = self._filter_line(view, query)
+
+    if new_query != query:
+      saved_selection = self.view.sel()[0]
+      self.view.replace(edit, all, new_query)
+
+    if len(self.view.sel()) == 1:
+      sel = self.view.sel()[0]
+      max_line, _ = view.rowcol(view.size())
+      should_goto_beginning = (sel.a == sel.b and sel.a == self.view.size() and
+        sel.a == len(str(max_line)))
+
+      if should_goto_beginning:
+        self.view.sel().clear()
+        self.view.sel().add(sublime.Region(0, 0))
+
+  def _filter_line(self, view, line):
+    replace = [('n', '0'), ('m', '1'), (',', '2'), ('.', '3'), ('j', '4'),
+      ('k', '5'), ('l', '6'), ('u', '7'), ('i', '8'), ('o', '9')]
+
+    for character, number in replace:
+      line = line.replace(character, number)
+
+    line = re.sub(r'[^\d]', '', line)
+    if line == '':
+      return ''
+
+    max_line, _ = view.rowcol(view.size())
+    max_line += 1 # because indexes starts with 1
+    if len(line) > len(str(max_line)):
+      line = line[0:len(str(max_line))]
+
+    return line
+
+class InputHelper(sublime_plugin.EventListener):
+  def on_modified_async(self, view):
+    panel = get_panel()
+    if panel == None or panel.get_panel().id() != view.id():
+      return
+
+    view.run_command('goto_line_filter_query')
+
+    view, query = panel.get_opener(), panel.get_current_text()
+    region = convert_query_to_region(view, query)
+    if region == None:
+      return None
+
+    region = view.full_line(region)
+    view.add_regions('goto_line_enhanced', [region], 'string')
+    view.show(region)
---
+++ readme.md
@@ -0,0 +1,80 @@
+# Sublime GotoLineEnhanced plugin
+
+Replacement for default sublime goto line.
+
+
+### Demo
+
+![Demo](https://raw.github.com/shagabutdinov/sublime-goto-line-enhanced/master/demo/demo.gif "Demo")
+
+
+### WARNING
+
+This plugin remap "goto line" functionality in unobvious way (because sublime
+does not allow to bind to "enter" in quick search panel). When you need to go to
+line hit "tab" instead of "enter".
+
+
+### Reason
+
+- Bright highlighting for line; even when typing fast it is always possible to
+see to which line jump will be made
+
+- Symbols mapping; it remaps keys from "n" to "p" to according "1" to "9", "n"
+for "0"; it is faster to hit symbols keys than numeric keys; it is also possible
+to enter line number with one hand (it is more comfortable for me)
+
+- Go to first char of line instead of default go to beginning of line
+
+- Does not allow to input symbols to avoid occasional keyhits
+
+### Installation
+
+This plugin is part of [sublime-enhanced](http://github.com/shagabutdinov/sublime-enhanced)
+plugin set. You can install sublime-enhanced and this plugin will be installed
+automatically.
+
+If you would like to install this package separately check "Installing packages
+separately" section of [sublime-enhanced](http://github.com/shagabutdinov/sublime-enhanced)
+package.
+
+
+### Features
+
+Goto first character of given line with bright highlighting and not-numeric
+keys.
+
+
+### Usage
+
+Hit keyboard shortcut to avoke panel, enter line number and hit "tab" to go to
+this line. You also can use following keys to enter numbers:
+
+| key | number |
+|-----|--------|
+| n   | 0      |
+| m   | 1      |
+| ,   | 2      |
+| .   | 3      |
+| j   | 4      |
+| k   | 5      |
+| l   | 6      |
+| u   | 7      |
+| i   | 8      |
+| o   | 9      |
+
+This values hard-binded to plugin and can be changed only thorugh source-code.
+
+
+### Commands
+
+| Description            | Keyboard shortcuts | Command palette                    |
+|------------------------|--------------------|------------------------------------|
+| Prompt goto line       | ctrl+g             | GotoLineEnhanced: Prompt goto line |
+| Goto line              | tab                |                                    |
+
+
+### Todo
+
+- ctrl+s for "select to line" feature
+- ctrl+d for "delete to line" feature
